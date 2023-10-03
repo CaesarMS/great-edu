6e06eb92fecbc1e464730dbefe05c6a191a3d98c
CaesarMS <caesar28997@gmail.com>
2023-10-03 21:29:53 +0700

    add solidity files

--- Profile.sol
+++ Profile.sol
@@ -1,32 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-library ScoreCalc {
-    function isPassed(
-        uint256 _score,
-        uint256 _minScore
-    ) external pure returns(bool) {
-        return _score > _minScore;
-    }
-}
+import "./ScoreCalc.sol";
 
 contract Profile {
     using ScoreCalc for uint256;
     uint256 public constant minScore = 75;
 
     struct Score {
+        uint128 subjectIndex;
+        uint128 score;
         string subjectName;
-        uint256 score;
     }
     mapping(address => Score[]) public userScore;
-    address public owner;
+    mapping(address => mapping(string => uint256)) public userSubjectIndex;
+
+    address[] public owner;
+    mapping(address => uint256) public ownerIndex;
+
     modifier onlyOwner {
-        require(msg.sender == owner, "unauthorized");
+        uint256 _ownerIndex = ownerIndex[msg.sender];
+
+        require(owner[_ownerIndex] == msg.sender, "unauthorized");
         _;
     }
 
     constructor(address _owner){
-        owner = _owner;
+        owner.push(_owner);
     }
 
     function addUser(
@@ -34,10 +34,45 @@ contract Profile {
         string calldata _subjectName,
         uint256 _score
     ) public onlyOwner {
+        uint256 index = getUserScoreLength(_user);
+        userSubjectIndex[_user][_subjectName] = index;
+
         userScore[_user].push(Score({
+            subjectIndex: uint128(index),
             subjectName: _subjectName,
-            score: _score
+            score: uint128(_score)
         }));
+    }
+
+    function isUserExist(address _user) public view returns(bool){
+        return userScore[_user].length > 0;
+    }
+
+    function isUserSubjectExist(
+        address _user,
+        string calldata _subjectName
+    ) public view returns(bool){
+        if(getUserScoreLength(_user) == 0) return false;
+
+        uint256 subjectIndex = userSubjectIndex[_user][_subjectName];
+
+        return keccak256(bytes(userScore[_user][subjectIndex].subjectName)) == keccak256(bytes(_subjectName));
+    }
+
+    function removeSubject(
+        address _user,
+        string calldata _subjectName
+    ) public onlyOwner {
+        require(isUserSubjectExist(_user, _subjectName), "not found");
+
+        uint256 subjectIndexToRemove = userSubjectIndex[_user][_subjectName];
+        Score memory userSubjectDataToMove = userScore[_user][getUserScoreLength(_user) - 1];
+
+        userScore[_user][subjectIndexToRemove] = userSubjectDataToMove;
+        userScore[_user][subjectIndexToRemove].subjectIndex = uint128(subjectIndexToRemove);
+
+        delete userSubjectIndex[_user][_subjectName];
+        userScore[_user].pop();
     }
 
     function getUserScoreLength(address _user) public view returns(uint256){
